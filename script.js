3fd709f2c81319f90af3a7542ca8ca163f9df20a
Bagi <roland.bagi007@gmail.com>
2023-11-10 14:20:15 +0100

    range

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const button = document.querySelector("button");
 
-
+const red = document.querySelector("input:first-child");
+const green = document.querySelector("input:nth-child(2)");
+const blue = document.querySelector("input:last-of-type");
+const circle = document.querySelector(".circle");
+const input = document.querySelectorAll("input");
+input.forEach(inp => {
+    inp.addEventListener("input", () =>{
+        circle.style.backgroundColor = `rgb(${red.value}, ${green.value}, ${blue.value})`;
+    })
+})
+/*
 button.onclick = () => {
     const red = document.querySelector("input:first-child");
     const green = document.querySelector("input:nth-child(2)");
@@ -9,3 +19,4 @@ button.onclick = () => {
 
     circle.style.backgroundColor = `rgb(${red.value}, ${green.value}, ${blue.value})`;
 }
+*/
